9f4a47caa37d8d55b9fb9363397ddfbee84fa6c5
John Damilola Ogungbure <jogungbure@gmail.com>
2023-04-03 11:04:29 +0100

    added link 4

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -9,6 +9,7 @@ export const Navbar = () => {
         <ul className={styles.navList}>
           <li>Link 1</li>
           <li>Link 2</li>
+          <li>Link 3</li>
         </ul>
       </div>
     </nav>
